8456c6884938cad99332bcf3ed99e7a46f7a72c6
faridimanzade <faridfi@code.edu.az>
2020-08-07 15:31:45 +0400

    Create,Delete,Edit Post, Full Front are ready

---
+++ Oxu.az/Oxu.az/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Oxu.az.Abstractions;
+using Oxu.az.Models;
+
+namespace Oxu.az.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly INewsRepository _newsRepository;
+
+        public HomeController(ILogger<HomeController> logger, INewsRepository newsRepository)
+        {
+            _logger = logger;
+            _newsRepository = newsRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var listOfPosts = _newsRepository.GetAllPosts();
+            return View(listOfPosts);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var post = _newsRepository.GetPost(id);
+
+            return View(post);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
